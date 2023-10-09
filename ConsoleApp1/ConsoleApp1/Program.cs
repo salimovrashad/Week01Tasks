076af35342f3627a2d6f04f5a4d953e98e3e0986
salimovrashad <selimovresad33@gmail.com>
2023-10-09 20:30:17 +0400

    add task files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            //Ədədin sadə və ya mürəkkəb olduğunu tapan alqoritm yazın.
+            //int eded = 13;
+
+            //if (eded < 2)
+            //{
+            //    Console.WriteLine("Eded ne sadedir ne murekkeb.");
+            //}
+            //else
+            //{
+            //    bool isSade = true;
+            //    for (int i = 2; i <= eded; i+=2)
+            //    {
+            //        if (eded % i == 0)
+            //        {
+            //            isSade = false;
+            //            break;
+            //        }
+            //    }
+
+            //    if (isSade)
+            //    {
+            //        Console.WriteLine("Eded sadedir.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Eded murekkebdir.");
+            //    }
+            //}
+
+
+            //Task 2
+            //Daxil olunan ədədin 2nin qüvvəti olub olmadığını tapan alqoritm yazın.
+            //Console.WriteLine("Eded daxil edin...");
+            //int numberOne = Convert.ToInt32(Console.ReadLine());
+            //int equal = 2;
+
+
+            //while (equal <= numberOne)
+            //{
+            //    if (equal != numberOne)
+
+            //    { equal *= 2; };
+
+            //    if (equal == numberOne)
+            //    {
+            //        Console.WriteLine("The number is power of 2");
+            //        break;
+            //    }
+            //}
+
+            //if (equal > numberOne)
+            //{
+            //    Console.WriteLine("It is not");
+            //}
+
+
+            //Task 3
+            //Ədədin mərtəbələri sayını tapan alqoritm yazın.
+            //Console.Write("Bir ədəd daxil edin: ");
+            //int eded = Convert.ToInt32(Console.ReadLine());
+
+            //int mertebelerinSayi = 0;
+
+            //while (eded != 0)
+            //{
+            //    eded /= 10;
+            //    mertebelerinSayi++;
+            //}
+            //Console.WriteLine(mertebelerinSayi);
+
+        }
+    }
+}
